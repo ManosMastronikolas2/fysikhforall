563e87c1441725199a918c3aee0b4c7cf28287e5
ManosMastronikolas2 <47667830+ManosMastronikolas2@users.noreply.github.com>
2020-04-23 14:07:28 +0300

    Add files via upload

---
+++ SHMEIWSEIS/metatropes.php
@@ -0,0 +1,66 @@
+<html>
+<head><META http-equiv=content-type content="text/html;charset=UTF-8">
+<meta http-equiv="cache-control" content="no-cache"> <!-- tells browser not to cache -->
+<meta http-equiv="expires" content="0"> <!-- says that the cache expires 'now' -->
+<meta http-equiv="pragma" content="no-cache"> <!-- says not to use cached stuff, if there is any --> 
+</head>
+<h2>
+<body background="ob012.jpg">
+<div left=500 top=500 
+<h3>ΜΕΤΑΤΡΟΠΕΣ ΜΟΝΑΔΩΝ
+<br><a href=help.htm target=help1>Βοήθεια</a>*********<a href=help.doc target=help2>Βοήθεια σε αρχείο Word</a><br>
+1. Μετατρέψτε σε m μέτρα:<br><form action="metatropes_res.php" method="post">
+<input type=text name=calc1 size=4 style="font-size: 20px;" value="105">cm=
+<input type=text name=ans1 size=4 style="font-size: 20px;" value="">
+<input type=hidden name=c1 value="1.05">m&nbspΧρησιμοποιείστε την τελεία για διαχωριστικό δεκαδικών.<br>
+<input type="submit" name="submit1" value="Έλεγχος απάντησης">
+ <br>_____________________________________________________________
+<br>
+2. Μετατρέψτε σε cm εκατοστόμετρα:<br><input type=text name=calc2 size=4 style="font-size: 20px;" value="51">m=
+<input type=text name=ans2 size=4 style="font-size: 20px;" value="">cm
+<input type=hidden name=c2 value="5100"><br>
+<input type="submit" name="submit2" value="Έλεγχος απάντησης">
+ <br>_____________________________________________________________
+<br>
+3. Μετατρέψτε σε min λεπτά:<br><input type=text name=calc3 size=4 style="font-size: 20px;" value="60">sec=
+<input type=text name=ans3 size=4 style="font-size: 20px;" value="">min
+<input type=hidden name=c3 value="1"><br>
+<input type="submit" name="submit3" value="Έλεγχος απάντησης">
+ <br>_____________________________________________________________
+<br>
+
+4. Μετατρέψτε σε sec δευτερόλεπτα:<br><input type=text name=calc4 size=4 style="font-size: 20px;" value="19">h=
+<input type=text name=ans4 size=4 style="font-size: 20px;" value="">sec
+<input type=hidden name=c4 value="68400"><br>
+<input type="submit" name="submit4" value="Έλεγχος απάντησης">
+<br>_____________________________________________________________
+<br>
+
+5. Μετατρέψτε σε h ώρες:<br><input type=text name=calc5 size=4 style="font-size: 20px;" value="1440">sec=
+<input type=text name=ans5 size=4 style="font-size: 20px;" value="">h
+<input type=hidden name=c5 value="0.4"><br>
+<input type="submit" name="submit5" value="Έλεγχος απάντησης">
+<br>_____________________________________________________________`
+
+<br>6. Αν η μάζα ενός σώματος είναι m=10gr και ο όγκος του V=1 cm<sup>3</sup>, πόση είναι η πυκνότητα του σώματος σε g/cm<sup>3</sup>;Απάντηση:<input type=text name=ans6 size=4 style="font-size: 20px;" value="">g/cm<sup>3</sup>
+<input type=hidden name=c6 value="10"><br>
+<input type="submit" name="submit6" value="Έλεγχος απάντησης">
+<br>_____________________________________________________________
+
+<br>7. Αν η πυκνότητα  ενός σώματος είναι ρ=50gr/cm<sup>3</sup> και ο όγκος του V=10 cm<sup>3</sup>, πόση είναι η μάζα του σώματος σε g;Απάντηση:<input type=text name=ans7 size=4 style="font-size: 20px;" value="">g
+<input type=hidden name=m7 value="500"><br>
+<input type="submit" name="submit7" value="Έλεγχος απάντησης">
+<br>_____________________________________________________________
+
+
+<br>8. Αν η μάζα  ενός σώματος είναι m=11gr και η πυκνότητα του ρ=1 g/cm<sup>3</sup>, πόσος είναι ο όγκος του σώματος σε cm<sup>3</sup>;Απάντηση:<input type=text name=ans8 size=4 style="font-size: 20px;" value="">cm<sup>3</sup>
+<input type=hidden name=v8 value="11"><br>
+<input type="submit" name="submit8" value="Έλεγχος απάντησης">
+<br>_____________________________________________________________
+
+<br>
+<input type="submit" name="submit" value="Έλεγχος όλων των απαντήσεων">
+
+</form>
+
+</html>
